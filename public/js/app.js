cb6f98407554729c5190c0a61d0cc88a21e7a5ef
BazerquA <a.bazerque@gmail.com>
2020-04-28 15:08:09 +0100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', () => {
+  
+  fetch('http://www.boredapi.com/api/activity/')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+            
+});
