a618ea09eb2cb51e0ab4ce1237f6685c487f27bd
BazerquA <a.bazerque@gmail.com>
2020-04-29 14:02:55 +0100

    random activities being displayed

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,11 @@
 //   $( "#Recreational" ).click(function() {
 //     alert( "Handler for .click() called." );
 //   });
-
-
-
 //   });
 
 const random = document.getElementById('random');
 random.addEventListener('click', () => {
-  alert("Handler for .click() called.");
+  //alert("Handler for .click() called.");
 
   const options = {
     method: "GET",
@@ -21,11 +18,26 @@ random.addEventListener('click', () => {
 
   fetch('http://www.boredapi.com/api/activity/', options)
     .then(res => res.json())
-    .then(res => {
-      console.log(res)
+    .then(res => {appendData(res);
+      //console.log(res)
     })
-    .catch(err => console.log(err));
+   
+    .catch(function (err) {
+      console.log('error: ' + err);
+    });
+    
+  function appendData(data) {
+    var mainContainer = document.getElementById("myRandomData");
+    
+      var p = document.getElementById("myRandomData");
+      p.innerHTML = "<b>"+'Activity: '+"</b>" + data.activity + "<br>" +
+                    "<b>"+'Type: '+"</b>" + data.type; + "<br>" +
+                    "<b>"+'Participants: '+"</b>" + data.participants;
+      mainContainer.appendChild(p);
+    
+  }
 });
+
 
 const type = document.getElementById('bytype');
 type.addEventListener('click', () => {
